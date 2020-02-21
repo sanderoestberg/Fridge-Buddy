e03b014f2bc41bde64b16d1e0e602e2864a8a6b0
Johanne Justesen <johannejpoulsen@gmail.com>
2020-02-21 11:26:57 +0100

    login startup

---
+++ js/login.js
@@ -0,0 +1,61 @@
+"use strict";
+
+// ========== GLOBAL VARIABLES ========== //
+const _userRef = _db.collection("users")
+
+// ========== FIREBASE AUTH ========== //
+// Listen on authentication state change
+firebase.auth().onAuthStateChanged(function (user) {
+  if (user) { // if user exists and is authenticated
+    userAuthenticated(user);
+  } else { // if user is not logged in
+    userNotAuthenticated();
+  }
+});
+
+function userAuthenticated(user) {
+  _currentUser = user;
+  hideTabbar(false);
+  init();
+  showLoader(false);
+}
+
+function userNotAuthenticated() {
+  _currentUser = null; // reset _currentUser
+  hideTabbar(true);
+  showPage("login");
+
+  // Firebase UI configuration
+  const uiConfig = {
+    credentialHelper: firebaseui.auth.CredentialHelper.NONE,
+    signInOptions: [
+      firebase.auth.EmailAuthProvider.PROVIDER_ID
+    ],
+    signInSuccessUrl: '#fridge'
+  };
+  // Init Firebase UI Authentication
+  const ui = new firebaseui.auth.AuthUI(firebase.auth());
+  ui.start('#firebaseui-auth-container', uiConfig);
+  showLoader(false);
+}
+
+// show and hide tabbar
+function hideTabbar(hide) {
+  let tabbar = document.querySelector('#tabbar');
+  if (hide) {
+    tabbar.classList.add("hide");
+  } else {
+    tabbar.classList.remove("hide");
+  }
+}
+
+// sign out user
+function logout() {
+  firebase.auth().signOut();
+  // reset input fields
+  document.querySelector('#name').value = "";
+  document.querySelector('#mail').value = "";
+  document.querySelector('#birthdate').value = "";
+  document.querySelector('#hairColor').value = "";
+  document.querySelector('#imagePreview').src = "";
+}
