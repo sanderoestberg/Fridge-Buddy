064696ad212951c58a14059e090e0a323ed41668
Sander <sanderoestberg@hotmail.com>
2020-02-24 12:48:52 +0100

    Add-menu +

--- js/login.js
+++ js/login.js
@@ -7,7 +7,7 @@ let _spaService = new SpaService("login");
 // ========== GLOBAL VARIABLES ========== //
 const _userRef = _db.collection("users")
 let _currentUser;
-const _madRef = _db.collection("movies");
+const _madRef = _db.collection("madvarer");
 let _madvarer;
 
 // ========== FIREBASE AUTH ========== //
@@ -23,7 +23,7 @@ firebase.auth().onAuthStateChanged(function (user) {
 function userAuthenticated(user) {
   _currentUser = user;
   hideTabbar(false);
-  //init();
+  init();
   //showLoader(false);
 }
 
@@ -70,4 +70,53 @@ function logout() {
   document.querySelector('#birthdate').value = "";
   document.querySelector('#hairColor').value = "";
   document.querySelector('#imagePreview').src = "";*/
+}
+
+
+// initialize movie references - all movies and user's favourite movies
+function init() {
+  // init user data and favourite movies
+  _userRef.doc(_currentUser.uid).onSnapshot({
+    includeMetadataChanges: true
+  }, function (userData) {
+    if (!userData.metadata.hasPendingWrites && userData.data()) {
+      _currentUser = {
+        ...firebase.auth().currentUser,
+        ...userData.data()
+      }; //concating two objects: authUser object and userData objec from the db
+      appendUserData();
+      appendFavMovies(_currentUser.favMad);
+      if (_madvarer) {
+        appendMadvarer(_madvarer); // refresh movies when user data changes
+      }
+      //showLoader(false);
+    }
+  });
+
+  // init all movies
+  _madRef.onSnapshot(function (snapshotData) {
+    _madvarer = [];
+    snapshotData.forEach(function (doc) {
+      let mad = doc.data();
+      mad.id = doc.id;
+      _madvarer.push(mad);
+    });
+    appendMadvarer(_madvarer);
+  });
+}
+
+function appendMadvarer(madvarer) {
+  let htmlTemplate = "";
+  for (let mad of madvarer) {
+    htmlTemplate += `
+      <article class="madvarer">
+      <div class="mad highlight">
+      <h4>${mad.title}</h4>
+      <p>${mad.holdbarhed}</p>
+      <img src="${mad.img}">
+    </div>
+    </article>
+    `;
+  }
+  document.querySelector('#add-menu-forslag').innerHTML = htmlTemplate;
 }
